9d38b21f209f4809b06c90224dd962a89e627588
Christian Lessig <christian.lessig@ecmwf.int>
2026-07-03 09:12:08 +0200

    Temporary patch for decoder *for fixed grids*: use of absolute target coords

--- src/weathergen/datasets/tokenizer_utils.py
+++ src/weathergen/datasets/tokenizer_utils.py
@@ -521,4 +521,9 @@ def get_target_coords_local(
     zi = 99
     a[..., (geoinfo_offset + zi) :] = target_coords[..., (geoinfo_offset + 2) :]
 
+    a[..., 98] = np.sin(coords[:, 0])
+    a[..., 97] = np.cos(coords[:, 0])
+    a[..., 96] = np.sin(coords[:, 1])
+    a[..., 95] = np.cos(coords[:, 1])
+
     return a
